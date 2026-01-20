49d40f1a4579c1e86fa3f211cb4e69b53640bf13
PowerGuy67890 <Marshalltanderson@gmail.com>
2026-01-20 18:54:23 -0500

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -8,6 +8,11 @@ export default function Projects(){
                 <p>My first real project was based around replicating a DND campaign I played with my friends.</p>
                 <p>I found an AI model and feed it information about the game world and mechanics, and then built a front end interface for users to interact with.</p>
                 <p>The model handles the information well and simulates the game successful. The only issue is long run times due to my PC not being strong enough to run the model well.</p>
+                <p>Here is the Github Repo of the project{" "} <a
+                href="https://github.com/PowerGuy67890/DND-project"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
                 <p>Here is a screenshot of my user interface:</p>
                 <img src={IMG_7758} alt="IMG_7758" className= "image"></img>
             </div>
