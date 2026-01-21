55d3f5941089cf0e82fc598033444d368fcdd4e5
PowerGuy67890 <Marshalltanderson@gmail.com>
2026-01-21 01:18:46 +0000

    Updated Projects component with GitHub link

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,15 +4,20 @@ export default function Projects(){
     return (
         <div className ="general"/*style={{padding: "2rem"}}*/>
             <div className= "text">
-                <h1>My first real project is an attempt to simulate a DND campaign I played with my friends.</h1>
+                <h1>My first real project!</h1>
+                <p>I made a simulation of a DND campaign I played with my friends</p>
                 <p>I made an stylized user interface and used calls to an AI model locally to process information</p>
                 <p>It works great, albiet a little slow due to the amount of data it processes.</p>
-                <p>Here is the Github repo:{" "} <a
-                href="https://github.com/PowerGuy67890/DND-project"
-                target="_blank"
-                rel="noopener noreferrer"
-                >
-        </a></p>
+                <p>
+                    Here is the Github repo:{" "}
+                    <a
+                        href="https://github.com/PowerGuy67890/DND-project"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                    PowerGuy67890/DND-project
+                    </a>
+                </p>
                 <p>Here is a screenshot of the user interface:</p>
                 <img src={IMG_7758} alt="IMG_7758" className= "image"></img>
             </div>
