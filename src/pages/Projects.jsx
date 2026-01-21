6422b864f9c9c3f50cf72f13f6b2a59be0a6bedc
PowerGuy67890 <Marshalltanderson@gmail.com>
2026-01-20 19:56:34 -0500

    Add GitHub repo link to project description
    Added a link to the GitHub repository for the project.

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -7,6 +7,12 @@ export default function Projects(){
                 <h1>My first real project is an attempt to simulate a DND campaign I played with my friends.</h1>
                 <p>I made an stylized user interface and used calls to an AI model locally to process information</p>
                 <p>It works great, albiet a little slow due to the amount of data it processes.</p>
+                <p>Here is the Github repo:{" "} <a
+                href="https://github.com/PowerGuy67890/DND-project"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+        </a></p>
                 <p>Here is a screenshot of the user interface:</p>
                 <img src={IMG_7758} alt="IMG_7758" className= "image"></img>
             </div>
